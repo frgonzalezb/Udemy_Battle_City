958503fe9c5e7821974f543424884eb26b174f93
Francisco <fr.gonzalezb@duocuc.cl>
2024-03-08 17:20:09 -0300

    Main class __init__ method + game_config file

---
+++ game_config.py
@@ -0,0 +1,12 @@
+'''Main game settings file.'''
+
+# Game screen settings
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 896
+
+# FPS settings
+FPS = 60
+
+# RGB Color definitions
+BLACK = (0, 0, 0)
+
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+import game_config as gc
+
+
+class Main:
+    '''Everything starts here.'''
+
+    def __init__(self) -> None:
+        '''Initialize the main game loop object.'''
+        pygame.init()
+
+        # Set the display settings
+        self.screen = pygame.display.set_mode(
+            (gc.SCREEN_WIDTH, gc.SCREEN_HEIGHT)
+        )
+        pygame.display.set_caption('Battle City Clone')
+
+        # A clock is needed to regulate the speed of the game...
+        # ...in order to standarize the game performance.
+        self.clock = pygame.time.Clock()
+
+        # A simple check for the main game loop
+        self.run = True
