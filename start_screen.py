786af7bca1be754f4d830f694e073c2baf914ee3
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-15 12:19:17 -0400

    Update start_screen.py

--- start_screen.py
+++ start_screen.py
@@ -16,7 +16,19 @@ class StartScreen:
         self.image = self.assets.start_screen
         self.rect = self.image.get_rect(topleft=(0, 0))
 
-        self.start_screen_active = False
+        # Option positions
+        self.option_position = [
+            (4 * gc.IMAGE_SIZE, 7.75 * gc.IMAGE_SIZE),
+            (4 * gc.IMAGE_SIZE, 8.75 * gc.IMAGE_SIZE),
+            (4 * gc.IMAGE_SIZE, 9.75 * gc.IMAGE_SIZE)
+        ]
+        self.token_index = 0
+        self.token_image = self.assets.start_screen_token
+        self.token_rect = self.token_image.get_rect(
+            topleft=self.option_position[self.token_index]
+        )
+
+        self.start_screen_active = True
 
     def input(self) -> None:
         for event in pygame.event.get():
@@ -32,3 +44,6 @@ class StartScreen:
 
     def draw(self, window: pygame.Surface) -> None:
         window.blit(self.image, self.rect)
+
+        if self.start_screen_active:
+            window.blit(self.token_image, self.token_rect)
