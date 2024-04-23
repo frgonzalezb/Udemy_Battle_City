08486baac6d3453d61446eb7b3ea3d91e502c838
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-23 17:34:08 -0400

    Update game_hud.py

--- game_hud.py
+++ game_hud.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.surface import Surface
+
 import game_config as gc
 
 
@@ -16,12 +18,16 @@ class GameHUD:
         # Player 1 lives and display
         self.is_player_1_active: bool = False
         self.player_1_lives: int = 0
-        self.player_1_lives_image: pygame.Surface | None = None
+        self.player_1_lives_image: pygame.Surface = self.display_player_lives(
+            self.player_1_lives, self.is_player_1_active
+        )
 
         # Player 2 lives and display
         self.is_player_2_active: bool = False
         self.player_2_lives: int = 0
-        self.player_2_lives_image: pygame.Surface | None = None
+        self.player_2_lives_image: pygame.Surface = self.display_player_lives(
+            self.player_2_lives, self.is_player_2_active
+        )
 
         # Level information
         self.level = 1
@@ -34,18 +40,12 @@ class GameHUD:
         # Update the number of enemies still remaining to spawn
         self.enemies = self.game.enemies
 
-        # Update the stage number image
-        if self.level != self.game.level_num:
-            self.level = self.game.level_num
-            self.level_image = self.display_stage_number(self.level)
-
         # Update the number of player lives available
         self.is_player_1_active = self.game.is_player_1_active
-
-        if not self.is_player_1_active:
-            return
-
-        if self.player_1_lives != self.game.player_1.lives:
+        if (
+            self.is_player_1_active and
+            self.player_1_lives != self.game.player_1.lives
+        ):
             self.player_1_lives = self.game.player_1.lives
             self.player_1_lives_image = self.display_player_lives(
                 self.player_1_lives,
@@ -53,18 +53,22 @@ class GameHUD:
             )
 
         self.is_player_2_active = self.game.is_player_2_active
-
-        if not self.is_player_2_active:
-            return
-
-        if self.player_2_lives != self.game.player_2.lives:
+        if (
+            self.is_player_2_active and
+            self.player_2_lives != self.game.player_2.lives
+        ):
             self.player_2_lives = self.game.player_2.lives
             self.player_2_lives_image = self.display_player_lives(
                 self.player_2_lives,
                 self.is_player_2_active
             )
 
-    def draw(self, window: pygame.Surface) -> None:
+        # Update the stage number image
+        if self.level != self.game.level_num:
+            self.level = self.game.level_num
+            self.level_image = self.display_stage_number(self.level)
+
+    def draw(self, window: Surface) -> None:
         """
         Draws the HUD elements on the screen.
         """
@@ -75,24 +79,22 @@ class GameHUD:
 
         self.draw_enemy_tanks_remaining(window)
 
-        if self.is_player_1_active:
-            window.blit(
-                self.player_1_lives_image,
-                (14.5 * gc.IMAGE_SIZE, 9.5 * gc.IMAGE_SIZE)
-            )
+        window.blit(
+            self.player_1_lives_image,
+            (14.5 * gc.IMAGE_SIZE, 9.5 * gc.IMAGE_SIZE)
+        )
 
-        if self.is_player_2_active:
-            window.blit(
-                self.player_2_lives_image,
-                (14.5 * gc.IMAGE_SIZE, 11 * gc.IMAGE_SIZE)
-            )
+        window.blit(
+            self.player_2_lives_image,
+            (14.5 * gc.IMAGE_SIZE, 11 * gc.IMAGE_SIZE)
+        )
 
         window.blit(
             self.level_image,
             self.level_image_rect
         )
 
-    def draw_enemy_tanks_remaining(self, window: pygame.Surface) -> None:
+    def draw_enemy_tanks_remaining(self, window: Surface) -> None:
         """
         Draws the little tank images on the HUD screen to represent the
         number of enemies still remaining to spawn in the game area.
@@ -113,33 +115,11 @@ class GameHUD:
             else:
                 window.blit(self.images['grey_square'], (x, y))
 
-    def display_stage_number(self, level: int) -> pygame.Surface:
-        """
-        Generates the stage level image, according to the level number
-        passed in.
-        """
-        width, height = gc.IMAGE_SIZE, gc.IMAGE_SIZE // 2
-        surface = pygame.Surface((width, height))
-        surface.fill(gc.RGB_BLACK)
-
-        if level < 10:
-            image_1 = self.images['num_0']
-        else:
-            num = str(level)[0]
-            image_1 = self.images[f'num_{num}']
-
-        surface.blit(image_1, (0, 0))
-        num = str(level)[-1]
-        image_2 = self.images[f'num_{num}']
-        surface.blit(image_2, (gc.IMAGE_SIZE // 2, 0))
-
-        return surface
-
-    def generate_hud_overlay_screen(self) -> pygame.Surface:
+    def generate_hud_overlay_screen(self) -> Surface:
         """
         Returns a fixed HUD overlay screen image for the game.
         """
-        overlay_screen = pygame.Surface(
+        overlay_screen = Surface(
             (gc.SCREEN_WIDTH, gc.SCREEN_HEIGHT)
         )
         overlay_screen.fill(gc.RGB_GREY)
@@ -163,12 +143,12 @@ class GameHUD:
             self,
             player_lives: int,
             is_player_active: bool
-            ) -> pygame.Surface:
+            ) -> Surface:
         """
         Shows the player lives icon and number on the screen.
         """
         width, height = gc.IMAGE_SIZE, gc.IMAGE_SIZE // 2
-        surface = pygame.Surface((width, height))
+        surface = Surface((width, height))
         surface.fill(gc.RGB_BLACK)
 
         if player_lives > 99:
@@ -177,6 +157,7 @@ class GameHUD:
         if not is_player_active:
             surface.blit(self.images['grey_square'], (0, 0))
             surface.blit(self.images['grey_square'], (gc.IMAGE_SIZE // 2, 0))
+            return surface
 
         if player_lives < 10:
             image = pygame.transform.rotate(self.images['life'], 180)
@@ -186,6 +167,28 @@ class GameHUD:
 
         surface.blit(image, (0, 0))
         num = str(player_lives)[-1]
+        image_2 = self.images[f'num_{num}']
+        surface.blit(image_2, (gc.IMAGE_SIZE // 2, 0))
+
+        return surface
+
+    def display_stage_number(self, level: int) -> Surface:
+        """
+        Generates the stage level image, according to the level number
+        passed in.
+        """
+        width, height = gc.IMAGE_SIZE, gc.IMAGE_SIZE // 2
+        surface = Surface((width, height))
+        surface.fill(gc.RGB_BLACK)
+
+        if level < 10:
+            image_1 = self.images['num_0']
+        else:
+            num = str(level)[0]
+            image_1 = self.images[f'num_{num}']
+
+        surface.blit(image_1, (0, 0))
+        num = str(level)[-1]
         image_2 = self.images[f'num_{num}']
         surface.blit(image_2, (gc.IMAGE_SIZE // 2, 0))
 
