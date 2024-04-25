927e9f0ecd9e67633648c6b81d4e543b556c196f
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-25 17:12:50 -0400

    Create scores.py

---
+++ scores.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.surface import Surface
+
+
+class ScoreBanner(pygame.sprite.Sprite):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def update(self) -> None:
+        return super().update()
+
+    def draw(self, window: Surface) -> None:
+        return
