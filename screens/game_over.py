3a09f6d2206f0051774a7305133b0be15a52dad6
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-26 16:08:29 -0400

    Update game_over.py

--- screens/game_over.py
+++ screens/game_over.py
@@ -1,2 +1,33 @@
 import pygame
+from pygame.surface import Surface
+from pygame.rect import Rect
+
 import game_config as gc
+
+
+class GameOver:
+
+    def __init__(self, game, assets) -> None:
+        self.game = game
+        self.assets = assets
+
+        self.image: Surface = self.assets.context_images['game_over']
+        self.width, self.height = self.image.get_size()
+        self.position: tuple[int, int] = (
+                gc.SCREEN_WIDTH // 2 - self.width // 2,
+                gc.SCREEN_HEIGHT // 2 + self.height // 2,
+            )
+        self.rect: Rect = self.image.get_rect(center=self.position)
+
+        self.timer: int = pygame.time.get_ticks()
+        self.is_active: bool = False
+
+    def activate(self):
+        self.is_active = True
+        self.timer: int = pygame.time.get_ticks()
+
+    def update(self) -> None:
+        ...
+
+    def draw(self, window: Surface) -> None:
+        ...
