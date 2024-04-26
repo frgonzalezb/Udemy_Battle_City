ad81b018b41d5e0cdffa346eea663dc37f2dbdb3
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-26 16:40:37 -0400

    Update game.py

--- game.py
+++ game.py
@@ -114,6 +114,7 @@ class Game:
         # Game active or game over
         self.is_active: bool = True
         self.game_on: bool = False
+        self.game_over: bool = False
 
     def input(self) -> None:
         """
@@ -210,7 +211,7 @@ class Game:
         if self.fade.is_fade_active:
             self.fade.draw(window)
 
-    def create_stage_transition(self) -> None:
+    def create_stage_transition(self, game_over: bool) -> None:
         if not self.score_screen.is_active:
             self.score_screen.timer = pygame.time.get_ticks()
             if self.is_player_1_active:
@@ -225,7 +226,7 @@ class Game:
                 )
             self.score_screen.update_basic_info(self.top_score, self.level_num)
         self.score_screen.is_active = True
-        self.score_screen.update()
+        self.score_screen.update(game_over)
 
     def change_level(self, player_1_score, player_2_score) -> None:
         self.level_num += 1
