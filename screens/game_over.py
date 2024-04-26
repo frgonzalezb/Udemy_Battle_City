ffdfb4240f303a7b2834a2cd92d674cfc0da3721
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-26 16:21:39 -0400

    Update game_over.py

--- screens/game_over.py
+++ screens/game_over.py
@@ -13,22 +13,39 @@ class GameOver:
 
         self.image: Surface = self.assets.context_images['game_over']
         self.width, self.height = self.image.get_size()
-        self.position: tuple[int, int] = (
+        self.rect_center: tuple[int, int] = (
                 gc.SCREEN_WIDTH // 2 - self.width // 2,
                 gc.SCREEN_HEIGHT // 2 + self.height // 2,
             )
-        self.rect: Rect = self.image.get_rect(center=self.position)
+        self.rect: Rect = self.image.get_rect(center=self.rect_center)
 
         self.timer: int = pygame.time.get_ticks()
         self.is_active: bool = False
 
     def activate(self):
         self.is_active = True
-        self.timer: int = pygame.time.get_ticks()
+        self.timer = pygame.time.get_ticks()
 
     def update(self) -> None:
-        if self.rect.y > gc.SCREEN_HEIGHT // 4 - self.height // 2:
+        """
+        Defines the animation behavior for the Game Over screen.
+        """
+        game_over_position: int = gc.SCREEN_HEIGHT // 4 - self.height // 2
+
+        if self.rect.y > game_over_position:
+            # Move image upwards
             self.rect.y -= 10
+        elif self.rect.y < game_over_position:
+            # Stop image in the desired position (screen center)
+            self.rect.y = game_over_position
+            self.timer = pygame.time.get_ticks()
+
+        if (
+            self.rect.y == game_over_position and
+            pygame.time.get_ticks() - self.timer >= 3_000
+        ):
+            self.is_active = False
+            self.game.create_stage_transition(True)
 
     def draw(self, window: Surface) -> None:
         window.blit(self.image, self.rect)
